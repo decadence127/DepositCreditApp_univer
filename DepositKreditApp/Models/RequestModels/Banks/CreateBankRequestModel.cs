9e68efb1e96adb6a571a8e8d1abad89f2469aaba
Mikhail Sitsko <masitsko@gmail.com>
2021-10-23 00:09:15 +0300

    Adding required fields. Fixing issue with incorrect credit status calculation

--- DepositKreditApp/Models/RequestModels/Banks/CreateBankRequestModel.cs
+++ DepositKreditApp/Models/RequestModels/Banks/CreateBankRequestModel.cs
@@ -1,9 +1,13 @@
-﻿namespace DepositKreditApp.Models.RequestModels.Banks
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DepositKreditApp.Models.RequestModels.Banks
 {
     public class CreateBankRequestModel
     {
+        [Required]
         public string BankTitle { get; set; }
 
+        [Required]
         public double Balance { get; set; }
     }
 }
