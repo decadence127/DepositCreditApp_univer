3e3b80fe7494f279bf81998880599d831be07f01
Mikhail Sitsko <masitsko@gmail.com>
2021-10-22 23:58:39 +0300

    Updating numeric min value

--- frontend/src/Components/Cashier/DepositAction.tsx
+++ frontend/src/Components/Cashier/DepositAction.tsx
@@ -1,55 +1,65 @@
-import { Button, Box, TextField } from "@mui/material";
+import {Button, Box, TextField} from "@mui/material";
 import React from "react";
-import { appendCash } from "../../queryHandlers/clientQuery";
+import {appendCash} from "../../queryHandlers/clientQuery";
 
 type Props = {
-  clientId: string;
-  reload: Function;
+    clientId: string;
+    reload: Function;
 };
-const DepositAction: React.FC<Props> = ({ reload, clientId }) => {
-  const [depositingData, setDepositingData] = React.useState({
-    pinCode: "",
-    moneyAmount: 0,
-  });
-  const depositHandler = async (e: React.MouseEvent): Promise<any> => {
-    const response = await appendCash(
-      depositingData.pinCode,
-      depositingData.moneyAmount,
-      clientId
+const DepositAction: React.FC<Props> = ({reload, clientId}) => {
+    const [depositingData, setDepositingData] = React.useState({
+        pinCode: "",
+        moneyAmount: 0,
+    });
+    const depositHandler = async (e: React.MouseEvent): Promise<any> => {
+        const response = await appendCash(
+            depositingData.pinCode,
+            depositingData.moneyAmount,
+            clientId
+        );
+        reload();
+        console.log(response);
+    };
+    return (
+        <>
+            <TextField
+                type="number"
+                InputProps={{
+                    inputProps: {
+                        max: 3_000_000_000, min: 1
+                    }
+                }}
+                onChange={(e) =>
+                    setDepositingData({
+                        ...depositingData,
+                        moneyAmount: Number.parseInt(e.target.value),
+                    })
+                }
+                placeholder="Введите кол-во денег"
+            />
+            <TextField
+                type="number"
+                sx={{marginTop: 1}}
+                InputProps={{
+                    inputProps: {
+                        max: 9999, min: 0
+                    }
+                }}
+                onChange={(e) =>
+                    setDepositingData({...depositingData, pinCode: e.target.value})
+                }
+                placeholder="Введите PIN-код"
+            />
+            <Button
+                sx={{marginTop: 3}}
+                onClick={depositHandler}
+                variant="contained"
+                color="success"
+            >
+                Пополнить баланс
+            </Button>
+        </>
     );
-    reload();
-    console.log(response);
-  };
-  return (
-    <>
-      <TextField
-        type="number"
-        onChange={(e) =>
-          setDepositingData({
-            ...depositingData,
-            moneyAmount: Number.parseInt(e.target.value),
-          })
-        }
-        placeholder="Введите кол-во денег"
-      />
-      <TextField
-        type="number"
-        sx={{ marginTop: 1 }}
-        onChange={(e) =>
-          setDepositingData({ ...depositingData, pinCode: e.target.value })
-        }
-        placeholder="Введите PIN-код"
-      />
-      <Button
-        sx={{ marginTop: 3 }}
-        onClick={depositHandler}
-        variant="contained"
-        color="success"
-      >
-        Пополнить баланс
-      </Button>
-    </>
-  );
 };
 
 export default DepositAction;
